b5b83840e943fd0704795c62577f90f749872cc6
Antonina Sych <antoninasych@gmail.com>
2024-10-23 11:17:23 +0100

    updated styles

--- src/components/FilterSidebar/FilterSidebar.jsx
+++ src/components/FilterSidebar/FilterSidebar.jsx
@@ -1,59 +1,84 @@
 import { useState } from 'react';
 import styles from './FilterSidebar.module.css';
 
+
 const FilterSidebar = ({ onFilter }) => {
     const [location, setLocation] = useState('');
     const [filters, setFilters] = useState({
         AC: false,
-        kitchen: false,
-        bathroom: false,
+        Automatic: false,
+        Kitchen: false,
+        TV: false,
+        Bathroom: false,
+    });
+
+    const [vehicleType, setVehicleType] = useState({
+        Van: false,
+        FullyIntegrated: false,
+        Alcove: false,
     });
 
     const handleLocationChange = (e) => setLocation(e.target.value);
+
     const handleFilterChange = (e) => {
         setFilters({ ...filters, [e.target.name]: e.target.checked });
     };
 
+    const handleVehicleTypeChange = (e) => {
+        setVehicleType({ ...vehicleType, [e.target.name]: e.target.checked });
+    };
+
     const handleSearch = () => {
-        onFilter({ location, filters });
+        onFilter({ location, filters, vehicleType });
     };
 
     return (
         <div className={styles.filterSidebar}>
             <h3>Location</h3>
-            <input
-                type="text"
-                value={location}
-                onChange={handleLocationChange}
-                placeholder="Enter location"
-                className={styles.inputField}
-            />
+            <div className={styles.locationWrapper}>
+                <svg className={styles.icon}>
+                    <use href="../../assets/images/icons.svg#icon-map"></use>
+                </svg>
+                <input
+                    type="text"
+                    value={location}
+                    onChange={handleLocationChange}
+                    placeholder="Kyiv, Ukraine"
+                    className={styles.inputFieldWithIcon}
+                />
+            </div>
 
             <h3>Vehicle equipment</h3>
-            <label>
-                <input
-                    type="checkbox"
-                    name="AC"
-                    checked={filters.AC}
-                    onChange={handleFilterChange}
-                /> AC
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="kitchen"
-                    checked={filters.kitchen}
-                    onChange={handleFilterChange}
-                /> Kitchen
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="bathroom"
-                    checked={filters.bathroom}
-                    onChange={handleFilterChange}
-                /> Bathroom
-            </label>
+            <div className={styles.filterGrid}>
+                {Object.keys(filters).map((filter) => (
+                    <label key={filter} className={`${styles.filterItem} ${filters[filter] ? styles.selected : ''}`}>
+                        <input
+                            type="checkbox"
+                            name={filter}
+                            checked={filters[filter]}
+                            onChange={handleFilterChange}
+                            className={styles.checkbox}
+                        />
+                        {filter}
+                    </label>
+                ))}
+            </div>
+
+            <h3>Vehicle type</h3>
+            <div className={styles.filterGrid}>
+                {Object.keys(vehicleType).map((type) => (
+                    <label key={type} className={`${styles.filterItem} ${vehicleType[type] ? styles.selected : ''}`}>
+                        <input
+                            type="checkbox"
+                            name={type}
+                            checked={vehicleType[type]}
+                            onChange={handleVehicleTypeChange}
+                            className={styles.checkbox}
+                        />
+                        {type}
+                    </label>
+                ))}
+            </div>
 
             <button className={styles.searchButton} onClick={handleSearch}>Search</button>
         </div>
